955ca1fddffae4ec81b53112d3dba4f9874693ad
ArVanDamme <arthur.avd69@gmail.com>
2023-11-10 16:56:02 +0100

    Update de l'architecture global du projet

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Banner from './components/Banner/index';
-import Companies from './components/Companies/index';
+import Companies from './components/Technologies/index';
 import Work from './components/Work/index';
 import Table from './components/Table/index';
 import Features from './components/Features/index';
 import Simple from './components/Simple/index';
-import Trade from './components/Trade/index';
+import Trade from './components/Classement/index';
 import Faq from './components/Faq/index';
 
 
